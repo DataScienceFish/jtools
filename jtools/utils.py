7b8ba530ecfec1742b3cfad7bcf6c446c09db564
dsfish <liumingshuo1017@gmail.com>
2025-08-21 11:22:38 +0800

    fix: abs path for data

--- jtools/utils.py
+++ jtools/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 from jtools import consts as C
 
@@ -7,7 +8,8 @@ def get_trading_dates(market, startdate: str, enddate: str):
     """获取交易日"""
     exdates = C.HOLIDAYS.get(market, 'SH')
     if startdate < "20241231":
-        all_hisdates = pd.read_csv(f'data/trddt_{market}.csv', header=None, dtype=str)
+        fp_his = os.path.join(os.path.dirname(__file__), f'data/trddt_{market}.csv')
+        all_hisdates = pd.read_csv(fp_his, header=None, dtype=str)
     else:
         all_hisdates = []
     all_bdates = pd.bdate_range(start=max(startdate, "20241231"), end=enddate).strftime("%Y%m%d").tolist()
